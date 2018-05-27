f1f8140dfebebd879a6dbe1764fdb7d7ac509a54
Joey Boivin <joeyboivin@gmail.com>
2018-05-27 15:25:52 -0400

    Ajout des 3 fichiers

--- ChangeRepertoire.py
+++ ChangeRepertoire.py
@@ -3,8 +3,12 @@
 # ce fichier permet de changer l'adresse de chaque information d'un rerpertoire
 # a un autre.
 
-with open('info_test.txt') as f:
+testFile = 'info_test.txt'
+trainFile = 'info_train.txt'
+validationFile = 'info_vc.txt'
+
+with open(validationFile) as f:
     newText=f.read().replace('/home/pub/ele-778/labo/labo2/BaseDonnees', '')
 
-with open('info_test.txt', "w") as f:
+with open(validationFile, "w") as f:
     f.write(newText)
