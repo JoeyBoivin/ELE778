4c981cb0311711242b686cb2009c018f135e103b
Joey Boivin <joeyboivin@gmail.com>
2018-05-27 15:20:42 -0400

    Ajoute pas de ligne

--- ChangeRepertoire.py
+++ ChangeRepertoire.py
@@ -3,15 +3,8 @@
 # ce fichier permet de changer l'adresse de chaque information d'un rerpertoire
 # a un autre.
 
-import fileinput
+with open('info_test.txt') as f:
+    newText=f.read().replace('/home/pub/ele-778/labo/labo2/BaseDonnees', '')
 
-# Ouverture d'un fichier en *lecture*:
-fichier = "ELE778_MLP\info_vc.txt"
-
-#pour chaque ligne qui contient la premiere adresse est rempacer par la seconde
-for line in fileinput.input(fichier,inplace= True):
-    line = line.replace("/home/pub/ele-778/labo/labo2/BaseDonnees/tidigits","/ELE778_MLP")
-    print line
-fileinput.input(fichier).close()
-
-print "All modifications are done"
+with open('info_test.txt', "w") as f:
+    f.write(newText)
