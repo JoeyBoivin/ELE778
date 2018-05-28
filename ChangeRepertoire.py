67a9d584475c88607d3c3badedeeda9fb9ef8a60
Julien <ju.burson@gmail.com>
2018-05-28 13:50:17 -0400

    Fais le tris des 3 fichier en meme temps et changement de l'adapation de l'adresse des données

--- ChangeRepertoire.py
+++ ChangeRepertoire.py
@@ -7,8 +7,25 @@ testFile = 'info_test.txt'
 trainFile = 'info_train.txt'
 validationFile = 'info_vc.txt'
 
+#Changement de l'adresse des informations de test
+with open(testFile) as f:
+    newText=f.read().replace('/home/pub/ele-778/labo/labo2/BaseDonnees/tidigits/', '')
+
+with open(testFile, "w") as f:
+    f.write(newText)
+
+
+#Changement de l'adresse des informations d'essai
+with open(trainFile) as f:
+    newText=f.read().replace('/home/pub/ele-778/labo/labo2/BaseDonnees/tidigits/', '')
+
+with open(trainFile, "w") as f:
+    f.write(newText)
+
+
+#Changement de l'adresse des informations de validation
 with open(validationFile) as f:
-    newText=f.read().replace('/home/pub/ele-778/labo/labo2/BaseDonnees', '')
+    newText=f.read().replace('/home/pub/ele-778/labo/labo2/BaseDonnees/tidigits/', '')
 
 with open(validationFile, "w") as f:
     f.write(newText)
